740df06a92b8d0e513440c98e3ac17d94997aaa4
requinDr <75610214+requinDr@users.noreply.github.com>
2024-05-07 11:36:44 +0200

    local current_line_idx

--- translate.py
+++ translate.py
@@ -27,7 +27,6 @@ script_fr_mem: list[str] = get_file_lines(script_source)
 sourceScriptIndent: list[str] = get_file_lines(script_source_indent)
 csv_missing = dict()
 csv_dict = get_csv(csv_name_or_url) if not creer_csv else None
-current_line_idx = 0 # index de la ligne du fichier de la map en cours de traitement
 
 
 def remplace_dans_script(indice: int, ligne: str, nbStartSpaces: int = -1):
@@ -39,48 +38,48 @@ def remplace_dans_script(indice: int, ligne: str, nbStartSpaces: int = -1):
 	script_fr_mem[indice] = format_line_to_steam(ligne, nbStartSpaces)
 
 
-def line_process(idx: int, current_line: str, og_lines: list[str], tr_lines: list[str]):
-	global current_line_idx
+def line_process(idx: int, current_line: str, og_lines: list[str], tr_lines: list[str], last_found_idx: int) -> int:
 	global csv_missing
 
-	# Si l'indice de la ligne est dans la première colonne du csv
-	# et qu'une traduction est renseignée, on remplace la ligne par la traduction
-	if not creer_csv and (idx + 1) in csv_dict:
+	isInCsv = not creer_csv and (idx + 1) in csv_dict
+	if isInCsv:
 		csv_row = csv_dict[idx + 1]
 		translation = csv_row[csv_columns[1]]
 		nbStartSpaces = int(csv_row[csv_columns[2]]) if csv_row[csv_columns[2]] != "" else -1
 
-		if translation.strip() != "":
+	# Si l'indice de la ligne est dans la première colonne du csv
+	# et qu'une traduction est renseignée, on remplace la ligne par la traduction
+	if isInCsv and translation.strip() != "":
 			remplace_dans_script(idx, translation, nbStartSpaces)
-			return
+			return last_found_idx
 	
 	# S'il n'y a que des espaces dans la ligne, on passe à la suivante
 	if current_line.strip() == "":
-		return
+		return last_found_idx
 
 	indice = find_og_line_idx(og_lines, current_line)
 	if indice is not None:
-		current_line_idx = indice
+		last_found_idx = indice
 		ligne_traduite = recupere_ligne_traduite(indice, tr_lines)
 		if ligne_traduite:
 			remplace_dans_script(idx, ligne_traduite)
 	else:
 		# si la ligne n'est pas trouvée, on cherche si elle est incluse dans une autre ligne
-		ligne_partielle = get_partial_translation(idx, og_lines, tr_lines, script_fr_mem, current_line_idx)
+		ligne_partielle = get_partial_translation(idx, og_lines, tr_lines, script_fr_mem, last_found_idx)
 		if ligne_partielle is not None:
 			remplace_dans_script(idx, ligne_partielle)
 		else:
 			csv_missing[idx + 1] = current_line
 	
-	if not creer_csv and (idx + 1) in csv_dict:
-		csv_row = csv_dict[idx + 1]
-		nbStartSpaces = csv_row[csv_columns[2]]
-		if nbStartSpaces != "":
+	if isInCsv and nbStartSpaces != "":
 			remplace_dans_script(idx, script_fr_mem[idx], int(nbStartSpaces))
+
+	return last_found_idx
 
 
 def create_steam_file():
 	total_lignes = len(script_fr_mem)
+	last_found_idx: int = 0
 	
 	for idx, ligne in enumerate(script_fr_mem):
 		progress(idx, total_lignes - 1, "Steam\t", BLUE)
@@ -94,7 +93,7 @@ def create_steam_file():
 				tr_file_path = os.path.join(dossier_sources_fr, current_file)
 				tr_lines = get_file_lines(tr_file_path)
 
-			line_process(idx, ligne, og_lines, tr_lines)
+			last_found_idx = line_process(idx, ligne, og_lines, tr_lines, last_found_idx)
 			
 		except Exception as e:
 			print(f"Erreur lors du traitement de la ligne {idx + 1}: {e}. Passage à la ligne suivante.")
